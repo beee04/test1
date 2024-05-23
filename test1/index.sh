1d2b5f818ae4f68dcee39fc28d4f46a9536ff955
Roshini040 <roshinisoundar@vm-01.qnty0cu2mw0ejl3sosvutclhjd.cx.internal.cloudapp.net>
2024-05-23 05:54:57 +0000

    first commit

---
+++ test1/index.sh
@@ -0,0 +1,13 @@
+if [ ! -d /var/lib/docker ]; then
+    echo "Installing Docker..."
+fi
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+else
+    echo "Docker already installed"
+fi
